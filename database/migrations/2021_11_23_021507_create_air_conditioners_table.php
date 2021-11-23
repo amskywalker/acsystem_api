0d7736a569569407881467e525a10a879220d48e
Adailton Moura <adailton@agilytecnologia.com.br>
2021-11-22 23:22:10 -0300

    Set up initial migrations

---
+++ database/migrations/2021_11_23_021507_create_air_conditioners_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAirConditionersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('air_conditioners', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('laboratory_id');
+            $table->string('brand');
+            $table->string('type');
+            $table->boolean('status');
+            $table->timestamps();
+            $table->foreign('laboratory_id')->references('id')->on('laboratories');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('air_conditioners');
+    }
+}
---
+++ database/migrations/2021_11_23_021647_create_laboratory_access_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLaboratoryAccessTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('laboratory_access', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id');
+            $table->foreignId('laboratory_id');
+
+            $table->date('date');
+            $table->time('hour');
+            $table->string('justification');
+
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('laboratory_id')->references('id')->on('laboratories');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('laboratory_access');
+    }
+}
